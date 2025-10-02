9888e43892699b9c16cf856db85ca97259f483bc
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-10-02 08:32:02 +0530

    sort array by 0,1,2 and stock price code upadaed

--- src/arrays/SortAnArrayof012.java
+++ src/arrays/SortAnArrayof012.java
@@ -2,27 +2,28 @@ package arrays;
 
 public class SortAnArrayof012 {
     public static int[] sortArray(int[] array) {
-        int zeros = 0, ones = 0, twos = array.length - 1, temp;
-        while (ones < twos) {
-            if (array[ones] == 0) {
-                temp = array[ones];
-                array[ones] = array[zeros];
-                array[zeros] = temp;
-                ones++;
-                zeros++;
-            } else if (array[ones] == 1) ones++;
-            else{
-                temp = array[ones];
-                array[ones] = array[twos];
-                array[twos] = temp;
-                twos--;
+        int low = 0, mid = 0, high = array.length - 1;
+        int temp;
+        while (mid <= high) {
+            if (array[mid] == 0) {
+                temp = array[low];
+                array[low] = array[mid];
+                array[mid] = temp;
+                low++;
+            } else if (array[mid] == 1) {
+                mid++;
+            } else {
+                temp = array[high];
+                array[high] = array[mid];
+                array[mid] = temp;
+                high--;
             }
         }
         return array;
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 1, 0, 0, 1, 2, 0};
+        int[] array = {1, 2, 1,1,0,0, 0, 0, 1, 2, 0};
         sortArray(array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
--- src/arrays/Stock.java
+++ src/arrays/Stock.java
@@ -2,20 +2,21 @@ package arrays;
 
 public class Stock {
     public static int maxProfit(int[] prices) {
-        int lowPrice = 0;
-        int profit = 0;
+        int low = 0, high = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < prices[lowPrice]) {
-                lowPrice = i;
-            } else {
-                profit = Math.max(profit, prices[i] - prices[lowPrice]);
+            if (prices[i] < prices[low]) {
+                low = i;
+                high = i;
+            }
+            if (prices[i] > prices[high]) {
+                high = i;
             }
         }
-        return profit;
+        return prices[high] - prices[low];
     }
 
     public static void main(String[] args) {
-        int[] prices = {7, 1, 0, 5, 3, 6, 4};
+        int[] prices = {7, 1, 5, 3, 6, 8, 4};
         System.out.println(maxProfit(prices));
     }
 }
