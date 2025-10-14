cd9d4defc9c4425eaebe04616af1247f112a45fd
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-10-14 18:53:59 +0530

    find dublicate elements

---
+++ src/neetcode/FinDublicateElements.java
@@ -0,0 +1,17 @@
+package neetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FinDublicateElements {
+    public static boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
