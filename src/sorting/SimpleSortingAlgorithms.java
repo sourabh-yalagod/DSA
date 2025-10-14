16115bf693423671b080284ec5422a4f4cc7d7d9
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-10-14 23:09:31 +0530

    insertion sort

--- src/sorting/SimpleSortingAlgorithms.java
+++ src/sorting/SimpleSortingAlgorithms.java
@@ -22,4 +22,16 @@ public class SimpleSortingAlgorithms {
         }
         return array;
     }
+    private static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+        return array;
+    }
 }
