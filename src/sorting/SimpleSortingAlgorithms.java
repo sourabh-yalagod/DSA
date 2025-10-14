34736385011e7cc1351e050df6693a66a2ae16ba
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-10-14 23:09:50 +0530

    selection sort

--- src/sorting/SimpleSortingAlgorithms.java
+++ src/sorting/SimpleSortingAlgorithms.java
@@ -34,4 +34,19 @@ public class SimpleSortingAlgorithms {
         }
         return array;
     }
+    private static int[] selectionSort(int[] array) {
+        int min = 0;
+        for (int i = 0; i < array.length; i++) {
+            min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+                }
+            }
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
 }
