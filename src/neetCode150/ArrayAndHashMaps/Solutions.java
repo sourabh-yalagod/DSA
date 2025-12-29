e47887ee79302f0fbee0f343eb5cfea35cbbd718
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-12-29 08:29:29 +0530

    valid anagram problem solved

--- src/neetCode150/ArrayAndHashMaps/Solutions.java
+++ src/neetCode150/ArrayAndHashMaps/Solutions.java
@@ -1,12 +1,14 @@
 package neetCode150.ArrayAndHashMaps;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Solutions {
     public static void main(String[] args) {
         int[] array = {1, 2, 2, 4, 5};
-        System.out.println(containsDuplicate(array));
+        System.out.println(validAnagram("caat", "aact"));
     }
 
     private static boolean containsDuplicate(int[] nums) {
@@ -16,5 +18,19 @@ public class Solutions {
             else set.add(nums[i]);
         }
         return false;
+    }
+
+    private static boolean validAnagram(String str1, String str2) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : str1.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch : str2.toCharArray()) {
+            if (!map.containsKey(ch)) return false;
+            int count = map.get(ch);
+            if (count == 1) map.remove(ch);
+            else map.put(ch, count - 1);
+        }
+        return map.isEmpty();
     }
 }
