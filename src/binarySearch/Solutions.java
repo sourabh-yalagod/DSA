739d2db342c88cd4434ccb7aec2b02a857e6ed38
sourabh-yalagod <sourabhofficial99804@gmail.com>
2026-01-03 02:15:30 +0530

    search in matrix

--- src/binarySearch/Solutions.java
+++ src/binarySearch/Solutions.java
@@ -3,7 +3,8 @@ package binarySearch;
 public class Solutions {
     public static void main(String[] args) {
         int[] array = {1, 2, 4, 5, 6, 10, 11};
-        System.out.println(binarySearch(array,11));
+        int[][]  matrix= {{1, 2, 4, 8}, {10, 11, 12, 13}, {14, 20, 30, 40}};
+        System.out.println(searchInMatrix(matrix, 14));
     }
 
     private static int binarySearch(int[] array, int target) {
@@ -14,6 +15,17 @@ public class Solutions {
             if (array[mid] == target) return mid;
             else if (array[mid] > target) high = mid - 1;
             else low = mid + 1;
+        }
+        return -1;
+    }
+
+    private static int searchInMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) return matrix[row][col];
+            else if (matrix[row][col] > target) col--;
+            else row++;
         }
         return -1;
     }
