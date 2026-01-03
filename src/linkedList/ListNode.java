3a36c90a8fdb47c505cec8ec7e739b4ce3d59ea0
sourabh-yalagod <sourabhofficial99804@gmail.com>
2026-01-03 19:36:08 +0530

    merge two sorted array

--- src/linkedList/ListNode.java
+++ src/linkedList/ListNode.java
@@ -3,5 +3,9 @@ package linkedList;
 public class ListNode {
     public int val;
     public ListNode next;
+    public ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
 }
 
--- src/linkedList/Solutions.java
+++ src/linkedList/Solutions.java
@@ -17,4 +17,33 @@ public class Solutions {
         }
         return prev;
     }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        ListNode dummy = new ListNode(0);
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                dummy.next = list1;
+                list1 = list1.next;
+            } else {
+                dummy.next = list2;
+                list2 = list2.next;
+            }
+            dummy = dummy.next;
+        }
+
+        while (list1 != null) {
+            dummy.next = list1;
+            dummy = dummy.next;
+            list1 = list1.next;
+        }
+        while (list2 != null) {
+            dummy = dummy.next;
+            dummy.next = list2;
+            list2 = list2.next;
+        }
+        return dummy.next;
+    }
 }
