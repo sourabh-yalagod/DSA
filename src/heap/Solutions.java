98628d2f1f4045168a26d17f3c5fb2858bca7c34
sourabh-yalagod <sourabhofficial99804@gmail.com>
2026-01-05 11:17:07 +0530

    kth largest element in stream

--- src/heap/Solutions.java
+++ src/heap/Solutions.java
@@ -3,7 +3,8 @@ package heap;
 import java.util.PriorityQueue;
 
 public class Solutions {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 
     private static int lastStone(int[] stones) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
@@ -15,5 +16,28 @@ public class Solutions {
             pq.add(large - small);
         }
         return pq.isEmpty() ? 0 : pq.poll();
+    }
+
+    class KthLargest {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        private int k = 0;
+
+        public KthLargest(int k, int[] nums) {
+            for (int ele : nums) {
+                pq.offer(ele);
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+            this.k = k;
+        }
+
+        public int add(int val) {
+            pq.add(val);
+            if (pq.size() > this.k) {
+                pq.poll();
+            }
+            return pq.isEmpty() ? 0 : pq.peek();
+        }
     }
 }
