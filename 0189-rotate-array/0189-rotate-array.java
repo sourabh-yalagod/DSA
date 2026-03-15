ad92e98b280b957eabb9f718041fa96c0afb1cfa
Sourabh Yalagod <151551473+sourabh-yalagod@users.noreply.github.com>
2026-03-15 10:14:14 +0530

    Time: 0 ms (100%), Space: 61 MB (91.9%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public void rotate(int[] array, int k) {
-        int n = array.length;
-        k = k % n;  // handle cases like k > n
-
-        reverse(array, 0, n - 1);
-        reverse(array, 0, k - 1);
-        reverse(array, k, n - 1);
+        int low = 0;
+        int high = array.length-1;
+        k = k%(high+1);
+        reverse(array,low,high);
+        reverse(array,0,k-1);
+        reverse(array,k,high);
     }
 
     private static void reverse(int[] array, int low, int high) {
@@ -18,3 +18,9 @@ class Solution {
         }
     }
 }
+//1,2,3,4,5,6,7
+
+//7,6,5,4,3,2,1
+//5,6,7,4,3,2,1
+
+//5,6,7,1,2,3,4 
