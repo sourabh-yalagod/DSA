bbed205204970d7f946acfe6ecdb41c285c8f0b1
Sourabh Yalagod <151551473+sourabh-yalagod@users.noreply.github.com>
2026-04-04 16:07:14 +0530

    Time: 0 ms (100%), Space: 42.7 MB (89.61%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,34 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    // public boolean isSameTree(TreeNode p, TreeNode q) {
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.add(p);
+    //     queue.add(q);
+    //     while (!queue.isEmpty()) {
+    //         TreeNode firstTreeNode = queue.poll();
+    //         TreeNode secondTreeNode = queue.poll();
+    //         if (firstTreeNode==null && secondTreeNode==null) continue;
+    //         if (firstTreeNode==null || secondTreeNode==null) return false;
+    //         if (firstTreeNode.val != secondTreeNode.val) return false;
+    //         queue.add(firstTreeNode.left);
+    //         queue.add(secondTreeNode.left);
+    //         queue.add(firstTreeNode.right);
+    //         queue.add(secondTreeNode.right);
+    //     }
+    //     return true;
+    // }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(p);
-        queue.add(q);
-        while (!queue.isEmpty()) {
-            TreeNode firstTreeNode = queue.poll();
-            TreeNode secondTreeNode = queue.poll();
-            if (firstTreeNode==null && secondTreeNode==null) continue;
-            if (firstTreeNode==null || secondTreeNode==null) return false;
-            if (firstTreeNode.val != secondTreeNode.val) return false;
-            queue.add(firstTreeNode.left);
-            queue.add(secondTreeNode.left);
-            queue.add(firstTreeNode.right);
-            queue.add(secondTreeNode.right);
-        }
-        return true;
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
